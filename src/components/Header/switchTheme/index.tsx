b3cd5ebc0e49bc6a193a2ea2c6690cdcd95dccf4
Asad Maraqa <67272415+asadmaraqa@users.noreply.github.com>
2022-07-04 19:31:26 +0300

    more edits

---
+++ src/components/Header/switchTheme/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faX } from '@fortawesome/free-solid-svg-icons'
+import { useTheme } from '../../../context/theme/themeContext'
+
+const SwitchTheme = ({ onClick }: { onClick: () => void }) => {
+  console.log(typeof onClick)
+  const { setCurrentTheme } = useTheme()
+
+  return (
+    <div className="header__toggle">
+      <FontAwesomeIcon icon={faX} size="1x" onClick={onClick} />
+      <ul>
+        <li>
+          <button onClick={() => setCurrentTheme('blue')}>Blue</button>
+        </li>
+        <li>
+          <button onClick={() => setCurrentTheme('red')}>Red</button>
+        </li>
+        <li>
+          <button onClick={() => setCurrentTheme('green')}>Green</button>
+        </li>
+        <li>
+          <button onClick={() => setCurrentTheme('original')}>Purple</button>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default SwitchTheme
